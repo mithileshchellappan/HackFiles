a10d9897ec8387e10e14855a6ab12f8e3f757410
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-04-18 14:15:12 +0530

    issue#1 fixed

--- hackfiles/lib/profile_ui.dart
+++ hackfiles/lib/profile_ui.dart
@@ -66,70 +66,72 @@ final kLightTheme = ThemeData(
 class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    ScreenUtil.init(context, height: 896, width: 414, allowFontScaling: true);
+    // ScreenUtil.init(context, height: 896, width: 414, allowFontScaling: true);
 
     var profileInfo = Expanded(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: kSpacingUnit.w * 10,
-            width: kSpacingUnit.w * 10,
-            margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
-            child: Stack(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: kSpacingUnit.w * 5,
-                  backgroundImage: AssetImage('assets/avatar.png'),
-                ),
-                Align(
-                  alignment: Alignment.bottomRight,
-                  child: Container(
-                    height: kSpacingUnit.w * 2.5,
-                    width: kSpacingUnit.w * 2.5,
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).accentColor,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Center(
-                      heightFactor: kSpacingUnit.w * 1.5,
-                      widthFactor: kSpacingUnit.w * 1.5,
-                      child: Icon(
-                        LineAwesomeIcons.pen,
-                        color: kDarkPrimaryColor,
-                        size: ScreenUtil().setSp(kSpacingUnit.w * 1.5),
+      child: SingleChildScrollView(
+              child: Column(
+          children: <Widget>[
+            Container(
+              height: kSpacingUnit.w * 10,
+              width: kSpacingUnit.w * 10,
+              margin: EdgeInsets.only(top: kSpacingUnit.w * 3),
+              child: Stack(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: kSpacingUnit.w * 5,
+                    backgroundImage: AssetImage('assets/avatar.png'),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Container(
+                      height: kSpacingUnit.w * 2.5,
+                      width: kSpacingUnit.w * 2.5,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).accentColor,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Center(
+                        heightFactor: kSpacingUnit.w * 1.5,
+                        widthFactor: kSpacingUnit.w * 1.5,
+                        child: Icon(
+                          LineAwesomeIcons.pen,
+                          color: kDarkPrimaryColor,
+                          size: ScreenUtil().setSp(kSpacingUnit.w * 1.5),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: kSpacingUnit.w * 2),
-          Text(
-            'Abcd Efg',
-            style: kTitleTextStyle,
-          ),
-          SizedBox(height: kSpacingUnit.w * 0.5),
-          Text(
-            'abcdefg@gmail.com',
-            style: kCaptionTextStyle,
-          ),
-          SizedBox(height: kSpacingUnit.w * 2),
-          Container(
-            height: kSpacingUnit.w * 4,
-            width: kSpacingUnit.w * 20,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(kSpacingUnit.w * 3),
-              color: Theme.of(context).accentColor,
-            ),
-            child: Center(
-              child: Text(
-                'Update your Name',
-                style: kButtonTextStyle,
+                ],
               ),
             ),
-          ),
-        ],
+            SizedBox(height: kSpacingUnit.w * 2),
+            Text(
+              'Abcd Efg',
+              style: kTitleTextStyle,
+            ),
+            SizedBox(height: kSpacingUnit.w * 0.5),
+            Text(
+              'abcdefg@gmail.com',
+              style: kCaptionTextStyle,
+            ),
+            SizedBox(height: kSpacingUnit.w * 2),
+            Container(
+              height: kSpacingUnit.w * 4,
+              width: kSpacingUnit.w * 20,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(kSpacingUnit.w * 3),
+                color: Theme.of(context).accentColor,
+              ),
+              child: Center(
+                child: Text(
+                  'Update your Name',
+                  style: kButtonTextStyle,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
 
